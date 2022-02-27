08609c2dcc40ed014d60cac1ac36e856b8ad68e3
greatlaki <greatlaki911@gmail.com>
2022-02-27 20:59:21 +0300

    I made a spider and added the code from scrapy-test_case

---
+++ rep_scraper/rep_scraper/spiders/repo_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class GitSpider(scrapy.Spider):
+    name = "github"
+    allowed_domains = ["github.com"]
+    start_urls = [
+        "https://github.com/scrapy",
+    ]
+
+    def parse(self, response, **kwargs):
+        yield response.follow(url=f'https://github.com/orgs/scrapy/repositories',
+                              callback=self.parse_repo)
+
+    def parse_repo(self, response):
+        for name in response.css("div.Box ul a.d-inline-block::attr(href)").getall():
+            yield response.follow(url=f'https://github.com{name}', callback=self.parse_repo_content)
+
+    def parse_repo_content(self, response):
+        yield {
+            'name-rep': response.css("strong.mr-2 a::text").get(),
+        }
